b1dce4a83e414ff51add2076f5958c1caaec0f61
Samuel Akinosho <39565075+LucidSamuel@users.noreply.github.com>
2025-03-05 13:19:08 +0400

    update

--- ms-pendulum/src/pages/index.tsx
+++ ms-pendulum/src/pages/index.tsx
@@ -62,7 +62,7 @@ export default function Home() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin="true"
+          crossOrigin="anonymous"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
